e738713ad25d35829a68ab08cbd356d743d07685
KLB <k.besser@tu-bs.de>
2021-02-03 12:02:09 +0100

    Add run script

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# Run all script to generate the results presented in the paper "On Fading
+# Channel Dependency Structures with a Positive Zero-Outage Capacity" (Karl-L.
+# Besser, Pin-Hsun Lin, Eduard Jorswieck)
+#
+# Copyright (C) 2021 Karl-Ludwig Besser
+# License: GPLv3
+
+echo "Running boundary example"
+python boundary.py --plot --export -x 8 -y 0
+
+echo "Running Rayleigh fading example"
+python rayleigh_fading.py --plot --export -x 0 5 0 -5 -5 -y 0 5 5 5 10
+
+echo "Running MRC script..."
+python3 maximum_ratio_combining.py --plot --export -s 0 -m 5
+
+echo "Running SC script..."
+python3 selection_combining.py --plot --export -s 10 -m 2 5 10
